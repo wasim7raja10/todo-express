dd088c4cf431a88222237d1760d7ffe190584c34
Wasim Raja <wasim7raja10@gmail.com>
2022-10-13 16:42:59 +0530

    cleaned server.js file

--- server.js
+++ server.js
@@ -1,32 +1,13 @@
 import express from "express"
-import bp from "body-parser"
 import morgan from "morgan"
 
 const app = express()
 
-app.use(bp.urlencoded({ extended: true, json: true }))
-app.use(bp.json())
+app.use(express.json())
 app.use(morgan('dev'))
 
-const db = []
-
 app.get('/', (req, res) => {
-  res.send('Hello World! my own server another text')
-})
-
-app.post('/todo', (req, res) => {
-  const newTodo = {
-    id: Date.now(),
-    text: req.body.text
-  }
-
-  db.push(newTodo)
-
-  res.json(newTodo)
-})
-
-app.get('/todo', (req, res) => {
-  res.json(db)
+  res.send('Hello World!')
 })
 
 app.listen(3000, () => {
